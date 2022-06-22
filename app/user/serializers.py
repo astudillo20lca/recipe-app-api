5249e90058463d26de973b2ee7be27c9f62bca81
MiguelAstudillo <miguel.astudillo@lca-net.com>
2022-06-22 09:55:52 +0200

    add serializer token

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,6 +1,9 @@
 """serializers for the web API view"""
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+from django.forms import PasswordInput
+from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
@@ -24,3 +27,34 @@ class UserSerializer(serializers.ModelSerializer):
             """create and return a user with encrypted password"""
 
             return get_user_model().objects.create_user(**validated_data)
+
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """serializer for the user auth token"""
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type':'password'},
+        trim_whitespace=False,)
+
+    def validate(self, attrs):
+        """validate and authenticate the user"""
+
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            msg = _('unable to authenticate with provided credentials.')
+            # standard way to raise errors with serializers
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
+
