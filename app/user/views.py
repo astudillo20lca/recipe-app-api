4a2df4721379ea0f35cba2900170e8192875973e
MiguelAstudillo <miguel.astudillo@lca-net.com>
2022-06-22 11:58:21 +0200

    add views for token auth

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,11 @@
 """views for the user API"""
 
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 from user.serializers import UserSerializer
+from user.serializers import AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
     """create a new user in the system"""
@@ -9,3 +13,8 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
+class CreateTokenView(ObtainAuthToken):
+    """create an new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES # optional
+    
