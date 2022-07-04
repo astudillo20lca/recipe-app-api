7d9d9641d5c01020a6b48f257ccb73e7120788e7
MiguelAstudillo <miguel.astudillo@lca-net.com>
2022-07-04 11:46:10 +0200

    add manager user views

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,6 @@
 """views for the user API"""
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -18,3 +18,16 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES # optional
     
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """manage the authenticated user"""
+    # user the same serialize as before
+    serializer_class = UserSerializer
+    # to verify user is who we think they are
+    authentication_classes = [authentication.TokenAuthentication,]
+    # what the user is allowed to do
+    permission_classes = [permissions.IsAuthenticated,]
+
+    # overwrite the get object.
+    def get_object(self):
+        """retrieve and return the auth user"""
+        return self.request.user
