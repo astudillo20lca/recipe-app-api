93071b6558b3df97e03a95547fd11cd051cd4498
MiguelAstudillo <miguel.astudillo@lca-net.com>
2022-07-04 14:28:37 +0200

    formatting

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """Tests for django admin"""
 
@@ -19,35 +20,34 @@ class AdminSiteTests(TestCase):
         self.client.force_login(self.admin_user)
 
         self.user = get_user_model().objects.create_user(
-        email = 'user@example.com',
-        password = 'testpass123',
-        name = 'test user'
+            email='user@example.com',
+            password='testpass123',
+            name='test user'
         )
 
-    def test_users_list(self):
-        """tests that users are listed on pange"""
+    def test_users_lists(self):
+        """tests that users are listed on page"""
         # url with the list of users
         url = reverse('admin:core_user_changelist')
-        
+
         # response object from get request authenticated as admin user
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-def test_edit_user_page(self):
-    """test the edit user page works"""
+    def test_edit_user_page(self):
+        """test the edit user page works"""
 
-    url = reverse('admin:core_user_change',args=[self.user.id])
-    res = self.client.get(url)
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
 
-    self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 200)
 
-def test_create_user_page(self):
-    """test the create user page works"""
-    
-    url = reverse('admin:core_user_add')
-    res = self.client.get(url)
+    def test_create_user_page(self):
+        """test the create user page works"""
 
-    self.assertEqual(res.status_code, 200)
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
 
+        self.assertEqual(res.status_code, 200)
